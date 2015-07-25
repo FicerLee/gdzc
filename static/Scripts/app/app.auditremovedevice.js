d4df30f7b28ab12b96982dcf5a89eb33ba3ee528
litao <ficerlee@gmail.com>
2015-07-25 21:32:34 +0800

    审核设备删除，部分完成

--- static/Scripts/app/app.auditremovedevice.js
+++ static/Scripts/app/app.auditremovedevice.js
@@ -3,21 +3,31 @@
         $container,
         company,
         category,
+        tag,
+        view,
+        getFilter,
         auditstatus,
         policy,
         init;
+    tag = '#auditremovedevice';
     company = require('app/app.company');
     category = require('app/app.devicecategory');
     auditstatus = require('app/app.auditstatus');
     policy = require('app/app.policy');
+    view = require('app/app.device.view');
     init = function (container) {
         $container = $(container);
         //显示单位信息
-        company.showComboTree('#auditremovedevice-company-combo');
+        company.showComboTree('#auditremovedevice-company');
         //显示设备类型
-        category.showComboTree('#auditremovedevice-category-combo');
+        category.showComboTree('#auditremovedevice-category');
         //显示审核状态
-        auditstatus.showCombo('#auditremovedevice-auditstatus-combo');
+        auditstatus.showCombo('#auditremovedevice-auditstatus');
+        //日期
+        $(tag + '-startdate').datebox();
+        $(tag + '-enddate').datebox({
+            value: new Date().toString('yyyy-MM-dd')
+        });
         //显示主数据信息
         $container.datagrid({
             columns: [[{
@@ -26,12 +36,16 @@
             }, {
                 field: 'status',
                 title: '审核状态',
-                width: 120
+                width: 80,
+                align: 'right',
+                styler: function (value) {
+                    return Utility.auditstatusStyle(value);
+                }
             }, {
                 field: 'assetno',
                 title: '资产编码',
                 width: 140,
-                sortable: true,
+                sortable: true
             }, {
                 field: 'deviceno',
                 title: '设备型号',
@@ -77,14 +91,30 @@
                 }
             },
             onRowContextMenu: function (e, rowIndex, rowData) {
+                $container.datagrid('selectRow', rowIndex);
                 $('#auditremovedevice-grid-contextmenu').menu('show', {
                     left: e.pageX,
-                    top: e.pageY
+                    top: e.pageY,
+                    onClick: function (item) {
+                        if (item.name == 'showDeviceInfo') {
+                            view.showInfo({
+                                deviceids: [rowData.deviceid]
+                            });
+                        } else if (item.name == 'doReSubmit') {
+
+                        } else if (item.name == 'doPass') {
+
+                        } else if (item.name == 'doReject') {
+
+                        } else if (item.name == 'doRemove') {
+
+                        }
+                    }
                 });
                 e.preventDefault();
             },
-            method: 'get',
-            url: 'data/auditremovedevice.html'
+            url: Utility.serverUrl + 'auditremovedevice/getlist',
+            queryParams: getFilter()
         });
         var menu = '#auditremovedevice-grid-contextmenu';
         /*检查菜单权限*/
@@ -111,10 +141,10 @@
         });
         /*检查菜单*/
         if (!policy.checkpolicy({
-            policyno: 'auditremovedevice-allowaudit',
-            policyname: '允许审核设备删除申请',
-            groupname: '审核删除设备'
-        })) {
+                policyno: 'auditremovedevice-allowaudit',
+                policyname: '允许审核设备删除申请',
+                groupname: '审核删除设备'
+            })) {
             var texts = [
                 '审核通过',
                 '审核退回',
@@ -125,16 +155,27 @@
                 if (item && item.target)
                     $(menu).menu('disableItem', item.target);
             }
-        };
+        }
         if (!policy.checkpolicy({
-            policyno: 'device-allowremove',
-            policyname: '新增删除设备申请',
-            groupname: '设备资料维护'
-        })) {
+                policyno: 'device-allowremove',
+                policyname: '新增删除设备申请',
+                groupname: '设备资料维护'
+            })) {
             var item = $(menu).menu('findItem', '再次提交设备删除申请...');
             if (item && item.target)
                 $(menu).menu('disableItem', item.target);
+        }
+    };
+    //筛选
+    getFilter = function () {
+        return {
+            key: $(tag + '-key').val(),
+            categoryid: $(tag + '-category').combotree('getValue'),
+            companyid: $(tag + '-company').combotree('getValue'),
+            statusid: $(tag + '-auditstatus').combobox('getValue'),
+            startdate: $(tag + '-startdate').datebox('getValue'),
+            enddate: $(tag + '-enddate').datebox('getValue')
         };
-    }
+    };
     exports.init = init;
 });
