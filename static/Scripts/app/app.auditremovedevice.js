8bb0528a3864e94489a430b41abec334ca0418ad
litao <ficerlee@gmail.com>
2015-07-26 21:57:51 +0800

    审核设备删除

--- static/Scripts/app/app.auditremovedevice.js
+++ static/Scripts/app/app.auditremovedevice.js
@@ -7,7 +7,13 @@
         view,
         getFilter,
         auditstatus,
+        fileexport,
+        doReSubmit,
+        doPass,
+        doReject,
+        doRemove,
         policy,
+        reloadGrid,
         init;
     tag = '#auditremovedevice';
     company = require('app/app.company');
@@ -15,6 +21,7 @@
     auditstatus = require('app/app.auditstatus');
     policy = require('app/app.policy');
     view = require('app/app.device.view');
+    fileexport=require('app/app.export');
     init = function (container) {
         $container = $(container);
         //显示单位信息
@@ -34,7 +41,7 @@
                 field: 'id',
                 checkbox: true
             }, {
-                field: 'status',
+                field: 'statusname',
                 title: '审核状态',
                 width: 80,
                 align: 'right',
@@ -55,16 +62,20 @@
                 field: 'categoryname',
                 title: '设备类型',
                 width: 120
+            },{
+                field:'deviceusername',
+                title:'设备使用人',
+                width:100
             }, {
                 field: 'assetcategoryname',
                 title: '资产类型',
                 width: 60
             }, {
-                field: 'companyname',
+                field: 'creatorcompanyname',
                 title: '部门单位',
                 width: 250
             }, {
-                field: 'assetbelong',
+                field: 'assetbelongname',
                 title: '资产归属',
                 width: 140
             }, {
@@ -76,6 +87,13 @@
                 title: '审核完成时间',
                 width: 120
             }]],
+            rowStyler:function(index,row){
+                if(row.statusname==='审核退回'){
+                    return {
+                        class:'text-red text-del'
+                    };
+                }
+            },
             isField: 'id',
             rownumbers: true,
             singleSelect: false,
@@ -85,11 +103,6 @@
             toolbar: '#auditremovedevice-toolbar',
             fit: true,
             striped: true,
-            rowStyler: function (index, row) {
-                if (row.status == "提交审核") {
-                    return 'background:#6293BB;color:#fff;';
-                }
-            },
             onRowContextMenu: function (e, rowIndex, rowData) {
                 $container.datagrid('selectRow', rowIndex);
                 $('#auditremovedevice-grid-contextmenu').menu('show', {
@@ -101,13 +114,27 @@
                                 deviceids: [rowData.deviceid]
                             });
                         } else if (item.name == 'doReSubmit') {
-//TODO:未完成
+                            //TODO:再次提交申请有一个BUG,可以反复提交申请
+                            doReSubmit({
+                                id:rowData.id,
+                                deviceid:rowData.deviceid
+                            }).done(reloadGrid);
                         } else if (item.name == 'doPass') {
-//TODO:未完成
+                            doPass({
+                                id:rowData.id
+                            }).done(reloadGrid);
                         } else if (item.name == 'doReject') {
-//TODO:未完成
+                            doReject({
+                                id:rowData.id
+                            }).done(reloadGrid);
                         } else if (item.name == 'doRemove') {
-//TODO:未完成
+                            $.messager.confirm('警告','是否确认将删除此设备申请？',function(r){
+                               if(r){
+                                   doRemove({
+                                       id:rowData.id
+                                   }).done(reloadGrid);
+                               }
+                            });
                         }
                     }
                 });
@@ -136,7 +163,28 @@
                 groupname: '审核删除设备'
             }),
             onClick: function () {
-//TODO:未完成
+
+                var fields=$(container).datagrid('getColumnFields');
+                var columns={};
+                $.each(fields,function(index,value){
+                   var col=$(container).datagrid('getColumnOption',value);
+                    columns[col.field]=col.title;
+                });
+                Utility.saveData({
+                    path:'auditremovedevice/exportfile',
+                    params:{
+                        columns:columns
+                    },
+                    success:function(res){
+                        fileexport.showDialog({
+                            title:'删除设备审核明细清单',
+                            rows:res.rows
+                        });
+                    },
+                    error:function(message){
+                        $.messager.alert('错误',message,'warning');
+                    }
+                })
             }
         });
         /*检查菜单*/
@@ -176,6 +224,88 @@
             startdate: $(tag + '-startdate').datebox('getValue'),
             enddate: $(tag + '-enddate').datebox('getValue')
         };
+    };
+    //再次提交审核
+    doReSubmit=function(options){
+        var deferred= $.Deferred();
+        Utility.saveData({
+            path:'auditremovedevice/submit',
+            params:{
+                id:options.id,
+                deviceid:options.deviceid,
+                creatorid:login.getLocalUser().usercode
+            },
+            success:function(res){
+                $.messager.alert('成功','该设备申请已再次提交成功','info');
+                deferred.resolve(res);
+            },
+            error:function(message){
+                $.messager.alert('错误',message,'warning');
+            }
+        });
+        return deferred.promise();
+    };
+    //审核删除
+    doRemove=function(options){
+        var deferred= $.Deferred();
+        Utility.saveData({
+            path:'auditremovedevice/remove',
+            params:{
+                id:options.id,
+                completerid:login.getLocalUser().usercode
+            },
+            success:function(res){
+                $.messager.alert('成功','该设备审核信息已成功删除','info');
+                deferred.resolve(res);
+            },
+            error:function(message){
+                $.messager.alert('错误',message,'warning');
+            }
+        });
+        return deferred.promise();
+    };
+    //审核通过
+    doPass=function(options){
+        var deferred= $.Deferred();
+        Utility.saveData({
+            path:'auditremovedevice/pass',
+            params:{
+                id:options.id,
+                completerid:login.getLocalUser().usercode
+            },
+            success:function(res){
+                $.messager.alert('成功','该设备已成功通过审核','info');
+                deferred.resolve(res);
+            },
+            error:function(message){
+                $.messager.alert('错误',message,'warning');
+            }
+        });
+        return deferred.promise();
+    };
+    //审核退回
+    doReject=function(options){
+        var deferred= $.Deferred();
+        Utility.saveData({
+            path:'auditremovedevice/reject',
+            params:{
+                id:options.id,
+                completerid:login.getLocalUser().usercode
+            },
+            success:function(res){
+                $.messager.alert('成功','该设备申请已经退回','info');
+                deferred.resolve(res);
+            },
+            error:function(message){
+                $.messager.alert('错误',message,'warning');
+            }
+        });
+        return deferred.promise();
+    };
+    //刷新表格数据
+    reloadGrid=function(){
+      if($(tag+'-grid'))
+      $(tag+'-grid').datagrid('reload',getFilter());
     };
     exports.init = init;
 });
